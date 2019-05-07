82573113efcfeea7f48f96b9ef169bfb5c36191f
Drain01 <drainey1@kent.edu>
2019-05-07 12:36:38 -0400

    Update 2) Demo Script.sql

--- 2) Demo Script.sql
+++ 2) Demo Script.sql
@@ -7,15 +7,14 @@ one result - one active request right now. Note that sp_whoisactive filters out 
 exec sp_whoisactive;
 
 
-SELECT DISTINCT
-    o.name AS [Object_Name]
-    ,o.type_desc as [Object_Type]
-FROM sys.sql_modules  m 
-INNER JOIN sys.objects  o 
-    ON m.object_id=o.object_id
-WHERE 
-    m.definition Like '%waitfor delay ''00:20:00''%'
-ORDER BY 2,1;
+SELECT   
+	DISTINCT SCHEMA_NAME(o.schema_id) as SchemaName
+	,o.name as ProcName
+FROM     syscomments AS c
+         INNER JOIN sys.objects AS o ON c.id = o.[object_id]
+         INNER JOIN sys.schemas AS s ON o.schema_id = s.schema_id
+WHERE    text LIKE '%waitfor delay ''00:20:00''%'
+ORDER BY  SCHEMA_NAME(o.schema_id),o.name;
 
 /* @Get_full_inner_text example. This will return either the stored proc or batch the current query is a part of. */
 exec sp_WhoIsActive @get_full_inner_text = 1;
@@ -78,11 +77,13 @@ exec sp_whoisactive @output_column_list = '[dd hh:mm:ss.mss][login_name][cpu][re
 
 select @schemaout
 
-/* Now we create the table itself. */
+/* Now we create the table itself. By default the table is a heap - if you are persisting data over a long period of time, best to add
+a PK / CI. */
 if not exists (Select 1 from sys.tables where  Schema_id = schema_id('Who') and name = 'SPActiveOutput')
 create table Who.SPActiveOutput 
 (
-	 [dd hh:mm:ss.mss] varchar(8000) NULL
+	 ID int identity(1,1) primary key clustered
+	 , [dd hh:mm:ss.mss] varchar(8000) NULL
 	 ,[login_name] nvarchar(128) NOT NULL
 	 ,[CPU] varchar(30) NULL
 	 ,[reads] varchar(30) NULL
@@ -94,7 +95,11 @@ create table Who.SPActiveOutput
  else
 	truncate table Who.SPActiveOutput;
 
- /* Now, we call sp_whoisactive and we point it to the table we just created.  */
+ /* Now, we call sp_whoisactive and we point it to the table we just created.
+ 
+ @destination_table supports 3 part names [DB].[Schema].Table], but it will assume the current DB and DBO if those parts aren't provided. 
+ 
+ It does NOT check to ensure the table exists before inserting or that columns match - that's up to you. Adding an ID column for a PK / CI is fine. */
 
  exec sp_whoisactive @output_column_list = '[dd hh:mm:ss.mss][login_name][cpu][reads][writes][sql_text][query_plan]', @get_plans = 1,
 	@destination_table = 'Who.SpActiveOutput';
